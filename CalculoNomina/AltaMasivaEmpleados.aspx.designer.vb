cdeff2c457a972d3a103829376c6fdad028b9982
Jorge Cantu <jorge.cramirez02@hotmail.com>
2024-10-07 18:05:25 -0600

    Ultima actualizacion de AltaMasivaEmpleados - 10/07/2024

--- CalculoNomina/AltaMasivaEmpleados.aspx.designer.vb
+++ CalculoNomina/AltaMasivaEmpleados.aspx.designer.vb
@@ -41,13 +41,13 @@ Partial Public Class AltaMasivaEmpleados
     Protected WithEvents ImportarFile As Global.System.Web.UI.WebControls.FileUpload
 
     '''<summary>
-    '''Control btnDescargarCSV.
+    '''Control btnDescargarXlsx.
     '''</summary>
     '''<remarks>
     '''Campo generado automáticamente.
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
-    Protected WithEvents btnDescargarCSV As Global.Telerik.Web.UI.RadButton
+    Protected WithEvents btnDescargarXlsx As Global.Telerik.Web.UI.RadButton
 
     '''<summary>
     '''Control btnCargarEmpleados.
@@ -66,6 +66,87 @@ Partial Public Class AltaMasivaEmpleados
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents lblInfo As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control panelSuccess.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents panelSuccess As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Control lblEmpleadosSuccess.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblEmpleadosSuccess As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control litSuccess.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents litSuccess As Global.System.Web.UI.WebControls.Literal
+
+    '''<summary>
+    '''Control panelErrores.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents panelErrores As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Control lblEmpleadosError.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblEmpleadosError As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control litErrores.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents litErrores As Global.System.Web.UI.WebControls.Literal
+
+    '''<summary>
+    '''Control panelErroresCatalogos.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents panelErroresCatalogos As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Control lblEmpleadosErrorCatalogos.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblEmpleadosErrorCatalogos As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control litErroresCatalogo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents litErroresCatalogo As Global.System.Web.UI.WebControls.Literal
 
     '''<summary>
     '''Control Mensaje_Resultado.
